c571002886ee4a29753656c2a605f8aabd87335f
triguz <trigun271289@hotmail.it>
2020-01-30 23:34:40 +0100

    Initial Commit
    Initial

---
+++ Assets/My Assets/MoreCubes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoreCubes : MonoBehaviour
+{
+	public GameObject cubePrefab;
+
+	public void Cube()
+	{	
+			Instantiate(cubePrefab);
+	}
+	
+}
